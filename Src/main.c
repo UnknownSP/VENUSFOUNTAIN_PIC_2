65025fbba67145e343110b141bca4f074f26bc96
UnknownSP <UnknownSP@izutsu.local>
2022-11-29 04:23:06 +0900

    modify

--- Src/main.c
+++ Src/main.c
@@ -9,6 +9,14 @@ void main(void) {
 
     Game_mode game_mode = 0;
 
+    static int ball_r_count = 0;
+    static int ball_l_count = 0;
+    static int m_r1_count = 0;
+    static int m_r2_count = 0;
+    static int m_l1_count = 0;
+    static int m_l2_count = 0;
+    const int on_count = 5;
+
     int i;
 
     init();
@@ -30,59 +38,104 @@ void main(void) {
         }
 
         if(game_mode == IN_GAME){
-            M_R = rcv_data[1] & 0b00000001;
             M_L = (rcv_data[1] >> 1) & 0b00000001;
+            if((rcv_data[1] & 0b00000001) == 1){
+                M_R = 1;
+                LED_3 = 1;
+            }else{
+                M_R = 0;
+                LED_3 = 0; 
+            }
+            if(((rcv_data[1] >> 1) & 0b00000001) == 1){
+                M_L = 1;
+                LED_4 = 1;
+            }else{
+                M_L = 0;
+                LED_4 = 0; 
+            }
         }else{
             M_R = 0;
             M_L = 0;
-        }
-        if(M_R == 1){
-            LED_3 = 1;
-        }else{
             LED_3 = 0;
-        }
-        if(M_L == 1){
-            LED_4 = 1;
-        }else{
             LED_4 = 0;
         }
+        //if(M_R == 1){
+        //    LED_3 = 1;
+        //}else{
+        //    LED_3 = 0;
+        //}
+        //if(M_L == 1){
+        //    LED_4 = 1;
+        //}else{
+        //    LED_4 = 0;
+        //}
 
         if(!BALL_SENS_R){
-            LED_1 = 1;
-            snd_data[1] |= 0b00000001;
+           ball_r_count++;
+            if(ball_r_count > on_count){
+                ball_r_count = on_count + 1;
+                LED_1 = 1;
+                snd_data[1] |= 0b00000001;
+            }
         }else{
+            ball_r_count = 0;
             LED_1 = 0;
             snd_data[1] &= 0b11111110;
         }
         if(!BALL_SENS_L){
-            LED_2 = 1;
-            snd_data[1] |= 0b00000010;
+           ball_l_count++;
+            if(ball_l_count > on_count){
+                ball_l_count = on_count + 1;
+                LED_2 = 1;
+                snd_data[1] |= 0b00000010;
+            }
         }else{
+            ball_l_count = 0;
             LED_2 = 0;
             snd_data[1] &= 0b11111101;
         }
 
-        if(!M_SENS1_R){
-            snd_data[1] |= 0b00000100;
+        if(M_SENS1_R){
+           m_r1_count++;
+            if(m_r1_count > on_count){
+                m_r1_count = on_count + 1;
+                snd_data[1] |= 0b00000100;
+            }
         }else{
+            m_r1_count = 0;
             snd_data[1] &= 0b11111011;
         }
-        if(!M_SENS2_R){
-            snd_data[1] |= 0b00001000;
+        if(M_SENS2_R){
+           m_r2_count++;
+            if(m_r2_count > on_count){
+                m_r2_count = on_count + 1;
+                snd_data[1] |= 0b00001000;
+            }
         }else{
+            m_r2_count = 0;
             snd_data[1] &= 0b11110111;
         }
-        if(!M_SENS1_L){
-            snd_data[1] |= 0b00010000;
+
+        if(M_SENS1_L){
+           m_l1_count++;
+            if(m_l1_count > on_count){
+                m_l1_count = on_count + 1;
+                snd_data[1] |= 0b00010000;
+            }
         }else{
+            m_l1_count = 0;
             snd_data[1] &= 0b11101111;
         }
-        if(!M_SENS2_L){
-            snd_data[1] |= 0b00100000;
+        if(M_SENS2_L){
+           m_l2_count++;
+            if(m_l2_count > on_count){
+                m_l2_count = on_count + 1;
+                snd_data[1] |= 0b00100000;
+            }
         }else{
+            m_l2_count = 0;
             snd_data[1] &= 0b11011111;
         }
-       
     }
 }
 
@@ -97,8 +150,9 @@ void init(void){
     ANSELB = 0x00;
   
     TRISA = 0xFF;
-    TRISB = 0x00;
-    WPUB = 0x00;
+    TRISBbits.TRISB5 = 0;
+    TRISBbits.TRISB3 = 0;
+    //WPUB = 0x00;
     TRISCbits.TRISC1 = 0;
     TRISCbits.TRISC5 = 0;
     TRISCbits.TRISC6 = 0;
@@ -108,6 +162,11 @@ void init(void){
     WDTCON = 0x13;
 
     I2C_init(addr);//アドレス
+
+    LED_1 = 0;
+    LED_2 = 0;
+    LED_3 = 0;
+    LED_4 = 0;
 }
 
 void interrupt  HAND(void){
